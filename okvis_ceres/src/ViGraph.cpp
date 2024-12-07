7fd136e24c62090724f24a93c135e8f892ad3e6f
Stefan Leutenegger <stefan.leutenegger@tum.de>
2024-12-07 12:39:20 +0100

    previous landmark initialisation status

--- okvis_ceres/src/ViGraph.cpp
+++ okvis_ceres/src/ViGraph.cpp
@@ -810,84 +810,64 @@ void ViGraph::updateLandmarks()
     double quality = 0.0;
     bool behind = false;
     double best_err = 1.0e12;
-    Eigen::Vector4d best_pos(0.0, 0.0, 0.0, 0.0);
-    double minD = 1.0e12;
-    Eigen::Matrix3d H = Eigen::Matrix3d::Zero();
-    if (num > 0) {
-      for (const auto &observation : it->second.observations) {
+    Eigen::Vector4d best_pos(0.0,0.0,0.0,0.0);
+    if(num>0){
+      Eigen::Array<double, 3, Eigen::Dynamic> dirs(3,num);
+      int o=0;
+      //bool bad = false;
+      for(const auto& observation : it->second.observations) {
         kinematics::Transformation T_WS, T_SCi;
         const StateId stateId(observation.first.frameId);
-        const State &state = states_.at(stateId);
+        const State & state = states_.at(stateId);
         T_WS = state.pose->estimate();
         T_SCi = state.extrinsics.at(observation.first.cameraIndex)->estimate();
-        kinematics::Transformation T_WCi = T_WS * T_SCi;
-        Eigen::Vector4d pos_Ci = T_WCi.inverse() * hp_W;
-        Eigen::Vector3d dir_W = (T_WCi.C() * pos_Ci.head<3>()).normalized();
+        kinematics::Transformation T_WCi = T_WS*T_SCi;
+        Eigen::Vector4d pos_Ci = T_WCi.inverse()*hp_W;
 
-        if (fabs(pos_Ci[3]) > 1.0e-12) {
-          pos_Ci = pos_Ci / pos_Ci[3];
+        if(fabs(pos_Ci[3])>1.0e-12) {
+          pos_Ci = pos_Ci/pos_Ci[3];
         }
-        if (pos_Ci[2] < 0.1) {
+        Eigen::Vector3d dir_W = (T_WCi.C()*pos_Ci.head<3>()).normalized();
+        if(pos_Ci[2]<0.1) {
           behind = true;
         }
-        if (pos_Ci[2] < 0.0) {
+        if(pos_Ci[2] < 0.0) {
           dir_W = -dir_W; // reverse!!
         }
 
         // consider only small reprojection errors
         Eigen::Vector2d err;
-        double *params[3];
+        double* params[3];
         params[0] = state.pose->parameters();
         params[1] = it->second.hPoint->parameters();
         params[2] = state.extrinsics.at(observation.first.cameraIndex)->parameters();
-        double *jacobians[3];
-        double *jacobians_minimal[3];
-        Eigen::Matrix<double, 2, 4> J1;
-        Eigen::Matrix<double, 2, 3> J1_minimal;
-        jacobians[0] = nullptr;
-        jacobians[1] = J1.data();
-        jacobians[2] = nullptr;
-        jacobians_minimal[0] = nullptr;
-        jacobians_minimal[1] = J1_minimal.data();
-        jacobians_minimal[2] = nullptr;
-        observation.second.errorTerm->EvaluateWithMinimalJacobians(params,
-                                                                   err.data(),
-                                                                   jacobians,
-                                                                   jacobians_minimal);
+        observation.second.errorTerm->Evaluate(params, err.data(), nullptr);
         const double err_norm = err.norm();
 
-        if (err_norm > 2.5) {
+        if(err_norm>2.5) {
           //bad = true;
           continue;
         }
 
-        if (fabs(pos_Ci[3]) > 1.0e-12) {
-          if (pos_Ci[2] < minD) {
-            minD = pos_Ci[2];
-          }
-        }
-
-        if (err_norm < best_err && pos_Ci.norm() > 0.0001) {
+        if(err_norm < best_err && pos_Ci.norm()>0.0001) {
           // remember best fit
           // if it was far away, leave it far away; but make sure it's in front
           // of the camera and at least at 10 cm...
-          const double dist = std::max(0.1, pos_Ci.norm());
-          best_pos.head<3>() = T_WCi.r() + dist * dir_W;
-          best_pos[3] = 1.0;
+          const double dist = std::max(0.1,pos_Ci.norm());
+          best_pos.head<3>() = T_WCi.r() + dist*dir_W;
+          best_pos[3]=1.0;
           best_err = err_norm;
         }
 
-        H += J1_minimal.transpose() * J1_minimal;
+        dirs.col(o) = dir_W;
+        ++o;
       }
-
-      Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> es;
-      es.compute(H);
-      const double s0 = sqrt(std::max(1.0e-12, es.eigenvalues()[0]));
-      quality = (minD - 3.0 / s0) / fabs(std::max(1.0e-12, minD));
-      if (behind) {
-        quality = 0.0;
-      }
-      if(quality > 0.15) {
+      Eigen::Array<double, 3, Eigen::Dynamic> dirso(3,o);
+      dirso = dirs.topLeftCorner(3,o);
+      Eigen::Vector3d std_dev =
+        ((dirso.colwise() - dirso.rowwise().mean()).square().rowwise().sum()).sqrt();
+      quality = std_dev.norm();
+      if(quality > 0.04) {
         isInitialised = true;
       } else {
         if(behind && best_pos.norm()>1.0e-12) {
@@ -898,7 +878,7 @@ void ViGraph::updateLandmarks()
     }
     // update initialisation
     it->second.hPoint->setInitialized(isInitialised);
-    it->second.quality = std::max(0.0, quality);
+    it->second.quality = quality;
   }
 }
 
